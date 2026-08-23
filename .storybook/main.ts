f257c97fdc16ffb00d5ee17c97fad10a4ddae64e
Vlad Mocanu <vladmoca90@gmail.com>
2026-08-23 01:41:05 +0100

    Fixed the main.ts

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,20 +1,19 @@
-import type { StorybookConfig } from '@storybook/nextjs-vite';
+import type { StorybookConfig } from "@storybook/nextjs-vite";
 
 const config: StorybookConfig = {
-  "stories": [
+  stories: [
     "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../app/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
-  "addons": [
+  addons: [
     "@chromatic-com/storybook",
     "@storybook/addon-vitest",
     "@storybook/addon-a11y",
     "@storybook/addon-docs",
-    "@storybook/addon-mcp"
+    "@storybook/addon-mcp",
   ],
-  "framework": "@storybook/nextjs-vite",
-  "staticDirs": [
-    "..\\public"
-  ]
+  framework: "@storybook/nextjs-vite",
+  staticDirs: ["..\\public"],
 };
 export default config;
